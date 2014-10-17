555494abca25fcb52295296ad4087880ae2c7fba
Eric Hosick <erichosick@gmail.com>
2014-10-17 13:31:25 -0700

    * glup process not actually minimizing

--- gulpfile.js
+++ gulpfile.js
@@ -65,6 +65,7 @@ gulp.task('build', function(){
          pkg.version))
       .pipe(gulp.dest('dist'))            // dump pkg.name + '.js'
       .pipe(rename(libName + '.min.js'))  // rename for minify
+      .pipe(uglify())                     // minify it
       .pipe(gulp.dest('dist'))            // dump pkg.name + '.min.js'
       .pipe(rename(libName + '.min.brow.js'))  // rename before browserify
       .pipe(browserified)                 // setup for browser support
--- tests/check-build.js
+++ tests/check-build.js
@@ -1,5 +1,5 @@
 describe("the modules were built correctly", function() {
    it ("should have the correct version", function() {
-      expect(m._["version-core"]).to.equal("0.1.14");
+      expect(m._["version-core"]).to.equal("0.1.15");
    });
 });
