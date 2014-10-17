cff1f1fb482ffbb99205ae766527ca1fa4ba2324
Eric Hosick <erichosick@gmail.com>
2014-10-17 12:43:29 -0700

    * shorten header.js

--- dist/mech-core.js
+++ dist/mech-core.js
@@ -5,9 +5,8 @@
 (function() {
 "use strict";
 
-var root = this; // Root becomes window (browser) or exports (server)
-var previous = root.m;
-var m = previous || { _ : {} }; // new module or merge with previous
+var root = this; // window (browser) or exports (server)
+var m = root.m || { _ : {} }; // new module or merge with previous
 var m_ = m._ || {}; // new sub-module or merge with pervious
 m["version-core"] = '0.1.12'; // New library OR to use existing library (m for example), please fork and add to that project.
 
@@ -15,7 +14,7 @@ m["version-core"] = '0.1.12'; // New library OR to use existing library (m for e
 if(typeof module !== 'undefined' && module.exports) {
   module.exports = m;
 } else {
-  this.m = m
+  root.m = m
 }
 
 // A number primitive that can not contain a primitive
