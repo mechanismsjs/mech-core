cef50f344ebb29cdd77fdbeb650e532bdd24145d
Eric Hosick <erichosick@gmail.com>
2016-08-12 22:20:55 -0700

    version bump

--- dist/mech-core.js
+++ dist/mech-core.js
@@ -1,5 +1,5 @@
 // mech-core.js
-// version: 0.2.2
+// version: 0.2.3
 // author: Eric Hosick <erichosick@gmail.com> (http://www.erichosick.com/)
 // license: MIT
 (function() {
@@ -8,7 +8,7 @@
 var root = this; // window (browser) or exports (server)
 var m = root.m || {}; // merge with previous or new module
 m._ = m._ || {}; // merge with pervious or new sub-module
-m._["version-core"] = '0.2.2'; // version set through gulp build
+m._["version-core"] = '0.2.3'; // version set through gulp build
 
 // export module for node or the browser
 if(typeof module !== 'undefined' && module.exports) {
